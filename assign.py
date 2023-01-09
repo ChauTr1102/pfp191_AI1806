964d50167d983272fce501beef8e4a93a4246141
ChauTr1102 <121995378+ChauTr1102@users.noreply.github.com>
2023-01-09 08:48:23 +0700

    Create assign.py
    AI1806

---
+++ assign.py
@@ -0,0 +1,13 @@
+x = 5
+if x > 2:
+    print('Bigger than 2')
+    print('Still bigger')
+print('done with 2')
+
+for i in range(5):
+    print(i)
+    if i > 2:
+        print('Bigger than 2')
+    print('done with i',i)
+print('all done')
+
